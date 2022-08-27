5aa332c62e56f1eb2753dbca7b1600c634c9d481
Sema Altıntaş <85832481+semaaltintas@users.noreply.github.com>
2022-08-27 22:05:28 +0300

    . (#2)
    * Hello world silindi.
    
    * .

--- gitDeneme/Program.cs
+++ gitDeneme/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 Console.WriteLine("Merhaba");
+Console.WriteLine("Naber");
+Console.WriteLine("İyidir");
 Console.WriteLine("tuba");
 Console.WriteLine("feride");
 Console.WriteLine("sema");
