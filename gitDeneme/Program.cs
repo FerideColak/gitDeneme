7dc3060ba1a50de612364c2ae723e9262b803bbe
altnts_sema@hotmail.com <390061@ogr.ktu.edu.tr>
2022-08-27 21:45:53 +0300

    Hello world silindi.

--- gitDeneme/Program.cs
+++ gitDeneme/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 Console.WriteLine("Merhaba");
+Console.WriteLine("Naber");
 Console.WriteLine("tuba");
 Console.WriteLine("feride");
 Console.WriteLine("sema");
